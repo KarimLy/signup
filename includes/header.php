079db82782aa4c24430564f2718d2a1315a864a7
Greg London <gregdlondon@gmail.com>
2016-06-13 20:32:23 -0500

    Updated Facebook SDK.

--- includes/header.php
+++ includes/header.php
@@ -58,15 +58,23 @@
 
 <body>
 
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=223751357754052";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<!-- old fb app id = 218261441523608 -->
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '223751357754052',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 
 <div id="top">
 
